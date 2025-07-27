a36c2ad1ccf86e950e308fd598d61e08e25f3151
DESKTOP-DNRTKFK\slev5 <hyunj1017@naver.com>
2025-07-27 14:48:46 +0900

    no message

--- src/main/java/edu/kh/portpolio/main/dto/ThemeDto.java
+++ src/main/java/edu/kh/portpolio/main/dto/ThemeDto.java
@@ -13,7 +13,18 @@ import lombok.Setter;
 @Builder
 public class ThemeDto {
 	private String backgroundColor;
+	private String backgroundColor2;
+	private String backgroundColor3;
 	private String color;
+	private String color2;
+	private String color3;
 	private String linkColor;
 	private String borderColor;
+	private String borderColor2;
+	private String borderColorTop;
+	private String borderColorBottom;
+	private String borderColorSide;
+	private String tableBorderColor;
+	private String sliderBorderColor;
+	private String sliderBorderColor2;
 }
